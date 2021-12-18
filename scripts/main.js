f15f05120485d65740fd17fffa81097aaffad390
LucasCelli <lucascelli@live.com>
2021-12-18 00:38:50 -0400

    update fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,1 @@
 // Javascript
-
-const form = document.querySelector('.form-qr');
-const inputURL = document.querySelector('.genqr');
-const finalResult = document.querySelector('.qrfinal');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    finalResult.src = `https://qrtag.net/api/qr_transparent.png?url=${inputURL.value}`;
-})
